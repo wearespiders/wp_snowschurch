9602d9db5a6b0183c32c0b6ad7ea7a7b47b62958
WeAreSpiders <wearespiders@gmail.com>
2013-05-06 02:09:32 -0700

    Subscribe plugin implemented

--- wp-content/themes/snowschurch/side-rightbar.php
+++ wp-content/themes/snowschurch/side-rightbar.php
@@ -4,12 +4,9 @@
         <div class="rightbar-content"><?php my_custom_submenu(); ?></div>
     </div>
     <div class="recentbar">
-    	<div class="rightbar-titleplate"><div class="rightbar-title">DEVOTEES COMMENTS</div></div>
+    	<div class="rightbar-titleplate"><div class="rightbar-title">JOIN US</div></div>
         <div class="rightbar-content">
-        <ul>
-        <li>one</li>
-        <li>two</li>
-        </ul>
+			<?php mailchimpSF_signup_form(); ?>
         </div>
     </div>
     <div class="rightbar-endscroll"></div>
